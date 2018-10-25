1aa1901c683ec123ae0e807f5e497f20cd16eda2
Javier Chacón <j.chacon.alvarez@gmail.com>
2018-10-25 16:05:23 +0200

    Added song _id push to User model.

--- routes/song.js
+++ routes/song.js
@@ -2,15 +2,21 @@ const express = require('express');
 const router = express.Router();
 
 const Song = require('../models/song');
+const User = require('../models/user')
 
 router.post('/newsong', (req, res, next) => {
   const { songName, noteHistory } = req.body;
   const newSong = Song({ songName, noteHistory });
   return newSong.save()
-    .then(() => {
-      Song.findOne({}).sort({ created_at: -1 })
-        .then((newSong) => {
-          res.status(201).json(newSong);
+    .then((song) => {
+      const { _id: songId } = song;
+      const { _id: userId } = req.session.currentUser;
+      User.findByIdAndUpdate(userId, { $push: { songs: songId } })
+        .then(() => {
+          Song.findById(songId)
+            .then((song) => {
+              res.status(201).json(song);
+            })
         });
     })
     .catch(next);
@@ -26,4 +32,3 @@ router.put('/:id', (req, res, next) => {
 });
 
 module.exports = router;
-``
