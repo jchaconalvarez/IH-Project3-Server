00143f7ceb69471beba817c9c1de2a6f430679ef
Laura R. Ojeda <rojedalaura@gmail.com>
2018-11-13 20:30:33 +0100

    Edit profile route

--- routes/profile.js
+++ routes/profile.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcrypt');
 
 const Users = require('../models/user');
 
@@ -16,8 +17,9 @@ router.get('/getProfile', (req, res, next) => {
 router.put('/:id', (req, res, next) => {
   const { _id: userId } = req.session.currentUser;
   const { email } = req.body;
-  Users.findByIdAndUpdate( userId, { email })
+  Users.findByIdAndUpdate(userId, { email })
     .then((user) => {
+      req.session.currentUser = user;
       res.status(200).json(user)
     })
 })
